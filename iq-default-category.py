eabc2068da2e05a9ecbcca683076c68e1e1304a4
Christopher Tolo <ctolo@sonatype.com>
2020-04-27 08:22:15 -0700

    Update iq-default-category.py

--- iq-default-category.py
+++ iq-default-category.py
@@ -7,7 +7,9 @@ iq_url = "http://localhost:8070"
 default = "Distributed"
 add_tag = {}
 
-orgs = iq_session.get(f'{iq_url}/api/v2/organizations').json()["organizations"]
+url = f'{iq_url}/api/v2/organizations'
+response = iq_session.get(url).json()
+orgs = response["organizations"]
 for org in orgs:
 	if org["id"] == "ROOT_ORGANIZATION_ID":
 		for tag in org["tags"]:
@@ -17,15 +19,20 @@ for org in orgs:
 				add_tag = {"tagId": tag_id }
 				
 if not bool(add_tag):
-	print(f"Did not find tag:{default}"); exit(1)
+	print(f"Did not find tag:{default}")
+	exit(1)
 
 print(f"Checking for applications without tags ...")
-apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
+url = f'{iq_url}/api/v2/applications'
+response = iq_session.get(url).json()
+apps = response["applications"]
+
 for app in apps:
 	if len(app["applicationTags"]) == 0:
 		print(f" -Adding tag to app: {app['publicId']}")
 		app["applicationTags"].append(add_tag)
 		app_id = app["id"]
+		
 		url = f"{iq_url}/api/v2/applications/{app_id}"
 		result = iq_session.put(url, json=app)
 		print(result.status_code == requests.codes.ok)
